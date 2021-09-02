54f839ffdacc2d14e3cc48c16e253648c27d2224
AlexanderKorobchenko <78229551+AlexanderKorobchenko@users.noreply.github.com>
2021-09-02 16:14:32 +0000

    Deploying to gh-pages from @ AlexanderKorobchenko/team-project_Filmoteka@f68006fb27ca51e60f71946843a111787867b985 🚀

--- js.b32607c5.js
+++ js.b32607c5.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"QvaY":[function(require,module,exports) {
 "use strict";require("../sass/main.scss");
 },{"../sass/main.scss":"clu1"}]},{},["QvaY"], null)
-//# sourceMappingURL=/team-project_Filmoteka/js.a4aa2c81.js.map
+//# sourceMappingURL=/team-project_Filmoteka/js.b32607c5.js.map
