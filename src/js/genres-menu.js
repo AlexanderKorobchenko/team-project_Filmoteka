b017de2604f12c65bf7712ff8250388377a581fa
Oleg Teslenko <orlantostello@gmail.com>
2021-09-06 00:16:09 +0300

    подкоректировал запрос

--- src/js/genres-menu.js
+++ src/js/genres-menu.js
@@ -1,34 +1,43 @@
 import menuTemplate from '../templates/genres-menu.hbs';
 import moviesList from '../templates/main-cards.hbs';
 import ApiService from './apiService.js';
+import filterGenres from './filterGenres.js';
 
 const finder = new ApiService();
 
 const genresMenuRef = document.querySelector('#genres_menu');
 const galleryList = document.getElementById('gallery');
 
-
 createGenresMenu();
 
 function createGenresMenu() {
-    const genresArray = JSON.parse(localStorage.getItem('Genres'));
-    genresArray.unshift({id: '', name:'none' });
-    genresMenuRef.insertAdjacentHTML('beforeend',menuTemplate(genresArray));
+  const genresArray = JSON.parse(localStorage.getItem('Genres'));
+  genresArray.unshift({ id: '', name: 'none' });
+  genresMenuRef.insertAdjacentHTML('beforeend', menuTemplate(genresArray));
 }
 genresMenuRef.addEventListener('input', onInput);
 function onInput(event) {
   console.dir(genresMenuRef);
-  if (
-    event.target[event.target.selectedIndex].value === ''
-  ) {
-
+  if (event.target[event.target.selectedIndex].value === '') {
     renderMoviesList(JSON.parse(localStorage.getItem('Popular')));
-    return
+    return;
   }
-    finder.searchType = 3;
+  finder.searchType = 3;
   finder.searchRequest = event.target[event.target.selectedIndex].value;
-  finder.searchMovies().then(data => renderMoviesList(data.results));
-
+  finder
+    .searchMovies()
+    .then(({ results }) => {
+      const genres = JSON.parse(localStorage.getItem('Genres'));
+      return results.map(result => ({
+        ...result,
+        release_date: result.release_date ? result.release_date.slice(0, 4) : result.release_date,
+        genres: filterGenres(genres, result),
+      }));
+    })
+    .then(data => {
+      renderMoviesList(data);
+    })
+    .catch(err => console.log(err));
 }
 
 function renderMoviesList(movie) {
