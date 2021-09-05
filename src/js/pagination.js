124723d9238315dc5a16fe870c2046b10d80ecb6
Olena-Kandibula <bakkara@ukr.net>
2021-09-05 19:02:40 +0300

    process

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,6 +1,5 @@
 import paginationTmp from '../templates/paginationTmp.hbs';
 import paginationTmplMobile from '../templates/paginationTmplMobile.hbs';
-// import './pagination-surf';
 
 window.onload = creatPagination();
 window.addEventListener("resize", screenWidthWindow);
@@ -31,8 +30,7 @@ function screenWidthWindow() {       //в зависимости от ширин
 
 function onAddButton() {
    buttonForDesktop.forEach((button) => {
-       button.classList.remove('visually-hidden')
-    //    li.classList.remove('visually-hidden')
+       button.classList.remove('visually-hidden');    
         }) 
 }
 
@@ -41,127 +39,59 @@ function onRemoveButton() {
             button.classList.add('visually-hidden')
         })
 }
+
 // Проба пагинации===================================================
-import './pagination-surf';
-const gallery = document.querySelector('#gallery');//куда рендерим картинки
-const pagination = document.querySelector('.pagination_container');
+
 let pagesEl = document.querySelectorAll('.pagination_item'); //кнопки пагинации
-let pagesSurfEl = document.querySelectorAll('.pagination_surf');
-console.log(pagesEl.length)
-console.log(pagesSurfEl)
-console.log(pagesEl[0])
 
-// const pageEnd = document.querySelector('.pagination_end');
-const pageEnd = document.querySelector('#btn9');
-const page2 = document.querySelector('#btn2');
-const page3 = document.querySelector('#btn3');
-// console.log(pageEnd)
-let pagesTotal = 6 // total.pages from API
+let pagesTotal = 1000 // total.pages from API
 
-// pageEnd.textContent = pagesTotal// записали последнюю кнопку
-let clearButton = pagesEl.length - 2;
-console.log(clearButton)// 9 кнопок для записи страниц
+let clearButton = pagesEl.length - 2;// 9 кнопок для записи страниц
+// console.log(clearButton)
 onWritesPageNumbers()
 
 function onWritesPageNumbers() { // записывает номера страниц в кнопки
+            
+    if (pagesTotal === clearButton) {            
+        for (let i = 1; i <= pagesTotal; i++) {
+              pagesEl[i].textContent = i;
+        }            
+    }
     
-    for (let page of pagesSurfEl) {
-        
-        if (pagesTotal <= clearButton) {
-            for (let i = 1; i <= pagesTotal; i++) {
-                pagesEl[i].textContent = i;
-                // console.log(page.textContent)
-            }
+    if (pagesTotal < clearButton) {                     
+        for (let i = 1; i <= pagesTotal; i++) {
+          pagesEl[i].textContent = i;                    
         }
-        if (pagesTotal < clearButton) {
-            for (let i = 1; i <= pagesTotal; i++) {
-                pagesEl[i].textContent = i;
-                // console.log(page.textContent)
-                console.log(pagesEl[pagesTotal]);
-                // for (let j = pagesTotal + 1; j <= clearButton; i++) {
-                //     pagesEl[j].classList.add('visually-hidden');
-                // }
-            }
-            // console.log(clearButton - pagesTotal)
-            // // let excessButtons = clearButton - pagesTotal // количество нnpm run devенужных кнопок
-            // // console.log(excessButtons)
-
-            // if (page.textContent = '0') {
-            //     console.log(page.textContent)
-            // }
-        //     buttonForDesktop.forEach((button) => {
-        //     button.classList.add('visually-hidden')
-        // })
+        for (let i = pagesTotal + 1; i <= clearButton; i++) {
+            pagesEl[i].classList.add('visually-hidden');
+        }                
+    }
+    
+    if (pagesTotal > clearButton) {
+        for (let i = 1; i <= clearButton-2; i++) {
+            pagesEl[i].textContent = i;
+            pagesEl[clearButton-1].textContent = '...';
+            pagesEl[clearButton].textContent = pagesTotal;
         }
     }
 }
 
-pagesEl.forEach(function(element) {
-    // console.log(element.textContent);
-//     for (let i = 2; i <= (pagesTotal - 1); i++) {
-//         element.textContent = i
-// }
+pagesEl.forEach(function(page) { 
+    page.addEventListener('click', getsPageNumber);   
 });
 
-function test2() {    
-    let pageNumber = +this.innerHTML;//надо отправить в fetch, получить новый массив и зарендерить
-    console.log(pageNumber)
-} 
 
-
-for (let page of pagesEl) {
-    page.addEventListener('click', test)
+function getsPageNumber() {// получает номер страницы для fetch запроса
+    
+  let pageNumber = +this.innerHTML;//надо отправить в fetch, получить новый массив и зарендерить
+    console.log(pageNumber)    
 }
+// еще сделать надо для mobile
+// еще сделать навесить current
 
-function test() {
-    console.log('test')
-    let pageNumber = +this.innerHTML;//надо отправить в fetch, получить новый массив и зарендерить
-    console.log(pageNumber)
+// навешиваю события на кнопки
+pagesEl[0].addEventListener('click', onPagination);
+
+function onPagination(e) {
+
 }
-
-
-// const pagesFromApi = []// создаем массив номеров кнопок из API
-// console.log(pagesFromApi)
-// for (let i = 1; i <= pagesTotal; i++) {
-//    pagesFromApi.push(i);
-// }
-// console.log(pagesFromApi)
-
-
-// pageEnd.textContent = pagesTotal// записали последнюю кнопку
-// page2.textContent = pagesFromApi[8]// записали последнюю кнопку
-
-// for (let page of pagesEl) {
-//    test1()
-// }
-
-// function test1(pagesEl) {
-//     for (let i = 1; i <= pagesTotal; i++) {
-//     page.textContent = i;
-// }
-//     // page.textContent = i;
-//     // page.textContent = pagesFromApi[8]
-//     // console.log('test')
-//     let pageNumber = +this.innerHTML;//надо отправить в fetch, получить новый массив и зарендерить
-//     console.log(pageNumber)
-// } 
-// for (let page of pagesEl) {
-//     page.addEventListener('click', test2)
-//     // console.log(page)
-// }
-
-// function test2() {    
-//     let pageNumber = +this.innerHTML;//надо отправить в fetch, получить новый массив и зарендерить
-//     console.log(pageNumber)
-// } 
-
-
-// for (let page of pagesEl) {
-//     page.addEventListener('click', test)
-// }
-
-// function test() {
-//     console.log('test')
-//     let pageNumber = +this.innerHTML;//надо отправить в fetch, получить новый массив и зарендерить
-//     console.log(pageNumber)
-// }
