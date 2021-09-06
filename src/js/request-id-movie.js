39835790e204b8eec93d75faa10585a9529e973e
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2021-09-07 01:05:20 +0300

    закрытие модалки

--- src/js/request-id-movie.js
+++ src/js/request-id-movie.js
@@ -17,34 +17,52 @@ function onSearchID(e) {
     .then((data) => {
       refs.modalWindow.innerHTML = moviesCard(data);
       openModalWindow();
-    
+
+      //вынести в новую функцию ниже
       const watchBtn = document.querySelector('.btn__watch');
       const popularFilm = JSON.parse(localStorage.getItem('Popular'));
       watchBtn.addEventListener('click', () => {
         // localStorage.setItem("Watched", JSON.stringify(data));
         // const watched = JSON.parse(localStorage.getItem('Watched'))
-        
+
         localStorage.setItem('watched', JSON.stringify(popularFilm.filter((film) => {
           if (film.id === e.target.offsetParent.id) {
             console.log(film.id)
             return film;
           }
         })))
-        
+
       });
-    
+
     })
     .catch(err => console.log(err));
-}
+};
 
+// ================= начало открытие/закрытие модалки =================
 function openModalWindow() {
   refs.modalWindow.classList.add('is-open'); //показали модалку
   document.querySelector(".close__button").addEventListener('click', closeModalWindow);
-  // window.addEventListener('keydown', onControlKey);
+  refs.modalWindow.addEventListener('click', onControlClick);
+  window.addEventListener('keydown', onControlKey);
+};
 
-}
+function onControlClick(event) {
+  if (event.target.classList.value === 'modal__backdrop') {
+    closeModalWindow();
+  }
+  return;
+};
+
+function onControlKey(event) {
+  if (refs.modalWindow.classList.value.includes('is-open')) {
+    if (event.keyCode === 27) {
+      closeModalWindow();
+    };
+  }
+};
 
 function closeModalWindow() {
   refs.modalWindow.classList.remove('is-open');
   document.querySelector('.image').src = '';
-}
+};
+// ================= конец открытие/закрытие модалки =================
