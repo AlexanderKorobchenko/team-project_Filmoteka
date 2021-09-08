9f2d2abdfa3fffd632032ccecb53e242eaacb00a
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2021-09-09 00:21:56 +0300

    bug-fix_search-input

--- src/js/input-movies-request.js
+++ src/js/input-movies-request.js
@@ -8,7 +8,7 @@ const finderQuery = new ApiService();
 const changeLoader = new Loader('.loader');
 const galleryList = document.getElementById('gallery');
 const searchForm = document.getElementById('search-form');
-// const main = document.querySelector('.main > .container');
+const pagination = document.querySelector('.pagination');
 
 const currentPageArray = JSON.parse(localStorage.getItem('Popular'));//пока используем популярные
 
@@ -17,60 +17,49 @@ searchForm.addEventListener('input', debounce(onSearchMovie, 800));
 function onSearchMovie(event) {
   event.preventDefault();
   changeLoader.addLoader();
+  clearGalleryContainer()
+  pagination.classList.remove('hidden');
 
   const searchQuery = event.target.value;
 
+  // для возврата популярных фильмов
   if (searchQuery === '') {
     renderMoviesList(currentPageArray);
     changeLoader.clearLoader();
-    // для возврата популярных фильмов
     return;
   }
+
   finderQuery.searchRequest = searchQuery;
   finderQuery.searchType = 1;
 
   finderQuery
     .searchMovies()
     .then(({ results }) => {
+      // Если ничего не найдено
       if (results.length === 0) {
-        renderError();        
+        const notFound = `<li class="error-item"><div class="error-img-notfound"></div></li>`;
+        galleryList.innerHTML = notFound;
+
+        pagination.classList.add('hidden');
         changeLoader.clearLoader();
-        // функция которая очищает разметку и выводит картинку - ничегот не найдено в мэйне
         return;
       }
-      changeLoader.clearLoader();
-      return objectTransformations(results);
-    })
-    .then(data => {      
+      // Если найдено, то рендерим
+      const data = objectTransformations(results);
       renderMoviesList(data);
       changeLoader.clearLoader();
-      return data;
     })
-    // .then(data => localStorage.setItem('Popular', JSON.stringify(data)))
     .catch(err => {
       console.log(err);
-      changeLoader.clearLoader();
     });
-  // changeLoader.clearLoader();
-}
 
-// function clearGalleryContainer() {
-//   galleryList.innerHTML = '';
-// }
+};
 
 function renderMoviesList(movie) {
   const markup = moviesList(movie);
   galleryList.innerHTML = markup;
-}
+};
 
-function renderError() {
-  console.log("Ашибачка");
-  const emptyLibrary = `<li class="error-item"><div class="error-img-notfound"></div></li>`;
-  // error.textContent = "This is a heading";
-  // galleryList.append(error);
-  galleryList.innerHTML = emptyLibrary;
-
-  return
-}
-
-// обсудить поиск по жанрам, функцию обработки результата для карточки, возврат популярного записи в локалсторидж, Пустая строка
+function clearGalleryContainer() {
+  galleryList.innerHTML = '';
+};
