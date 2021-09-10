9e0922594c7b8e357f7230fd33ec085999dbef31
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-09-10 21:08:16 +0300

    add clear input on click logo

--- src/js/clearInput.js
+++ src/js/clearInput.js
@@ -1,7 +1,12 @@
 const clearInput = document.getElementById('btn-home');
 const input = document.querySelector('.search__input');
+const homePage = document.getElementById('logo-home');
 
 clearInput.addEventListener('click', function () {
+    input.value = '';
+});
+
+homePage.addEventListener('click', function () {
     input.value = '';
 });
 
